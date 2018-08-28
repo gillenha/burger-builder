3fe548e92b404558dafcef4a189d18cb19664a9f
gillenha <gillen08@gmail.com>
2018-08-27 20:09:29 -0400

    final commits

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,39 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Auxil from '../../../hoc/Auxil';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-
-	const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+	// this could be a functional component,
+	// doesn't have to be a class
+	componentWillUpdate() {
+		console.log('[OrderSummary] will update')
+	}
+	
+	render() {
+		const ingredientSummary = Object.keys(this.props.ingredients)
 		.map(igKey => {
 			return (
 				<li key={igKey}>
-					<span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+					<span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
 				</li> );
 		} );
-	let orderPrice = props.price.toFixed(2);
-	let salesTax = orderPrice * 0.08;
-	let orderTotal = Number(salesTax) + Number(orderPrice);
-	return (
-		<Auxil>
-			<h3>Your Order:</h3>
-			<p>A delicious Burger with the following ingredients:</p>
-			<ul>
-				{ingredientSummary}
-			</ul>
-			<p>Order Price: &#36;{orderPrice}</p>
-			<p>Sales tax: &#36;{salesTax.toFixed(2)}</p>
-			<p><strong>Your total: &#36;{orderTotal.toFixed(2)}</strong></p>
-			<p>Continue to Checkout?</p>
-			<Button 
-				btnType="Danger" 
-				clicked={props.purchaseCancelled}>CANCEL</Button>
-			<Button 
-				btnType="Success" 
-				clicked={props.purchaseContinued}>CONTINUE</Button>
-		</Auxil>
-	);
+
+		let orderPrice = this.props.price.toFixed(2);
+		let salesTax = orderPrice * 0.06;
+		let orderTotal = Number(salesTax) + Number(orderPrice);
+			return (
+			<Auxil>
+				<h3>Your Order:</h3>
+				<p>A delicious Burger with the following ingredients:</p>
+				<ul>
+					{ingredientSummary}
+				</ul>
+				<p>Order Price: &#36;{orderPrice} 
+				<br />
+				Sales tax: &#36;{salesTax.toFixed(2)}</p>
+				<p><strong>Your total: &#36;{orderTotal.toFixed(2)}</strong></p>
+				<p>Continue to Checkout?</p>
+				<Button 
+					btnType="Danger" 
+					clicked={this.props.purchaseCancelled}>CANCEL</Button>
+				<Button 
+					btnType="Success" 
+					clicked={this.props.purchaseContinued}>CONTINUE</Button>
+			</Auxil>
+		);
+	}
 };
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Modal.css';
 import Auxil from '../../../hoc/Auxil';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-	<Auxil>
-		<Backdrop 
-			show={props.show}
-			clicked={props.modalClosed} />
-			<div 
-				className={classes.Modal}
-				style={{
-					transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-					opacity: props.show ? '1' : '0'
-				}} >
-				{props.children}
-			</div>
-	</Auxil>
-)
+class Modal extends Component {
+	
+	shouldComponentUpdate(nextProps, nextState) {
+		return nextProps.show !== this.props.show;
+	}
 
-export default modal;
+	componentWillUpdate() {
+		console.log('[Modal.js] will update')
+	}
+
+	render() {
+		return (
+		<Auxil>
+			<Backdrop 
+				show={this.props.show}
+				clicked={this.props.modalClosed} />
+				<div 
+					className={classes.Modal}
+					style={{
+						transform: this.props.show ? 'translateY(-100px)' : 'translateY(-100vh)',
+						opacity: this.props.show ? '1' : '0'
+					}} >
+					{this.props.children}
+				</div>
+		</Auxil>
+		);
+	}
+}
+
+export default Modal;
